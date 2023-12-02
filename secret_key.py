2d73d28c0a75431e4b1bea78a50452e36a1daeb3
pallavi8386 <pallavi.8386@gmail.com>
2023-12-02 18:18:02 +0530

    Remove sensitive API key file

--- secret_key.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-#api_key = "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"
-
-api_key = "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"
